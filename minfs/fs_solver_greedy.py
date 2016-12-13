3c8a49d7c15f372aaa84c21482bd155a6dfc1f13
Shannon Fenn <shannon.fenn@gmail.com>
2016-12-13 14:01:54 +1100

    [feature] greedy solver

---
+++ minfs/fs_solver_greedy.py
@@ -0,0 +1,16 @@
+import numpy as np
+import minfs.utils as utils
+
+
+def greedy_minfs(X, y):
+    X = X.astype(np.uint8)
+    y = y.astype(np.uint8)
+
+    if np.all(y) or not np.any(y):
+        # constant target - no solutions
+        return []
+
+    C, Np = utils.packed_coverage_map(X, y)
+    fs = set()
+    utils.greedy_repair(C, Np, fs)
+    return list(fs)
